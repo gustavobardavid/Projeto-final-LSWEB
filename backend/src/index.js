4411e77c5e67122988f38b7f18e67b304640c1dc
Gustavo <Gustavo.cesariosilva15@gmail.com>
2023-06-24 20:31:18 -0300

    Criando router e pasta models para lidar com o banco de dados

--- backend/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/' , (request, response) => response.status(200).send('olá, mundo'))
-
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
-})
